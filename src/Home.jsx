35f7e5178de374bdd78d5a4c6bedddfe32a4266f
affanmehmood <affanmehmood32@gmail.com>
2020-08-13 11:50:32 +0500

    compleated all pages

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./index.css";
 import HomeAbout from "./ReusableCompnents/home-about";
 import image from "./images/video.svg";
+
 const Home = () => {
   const msg1 = "Investigate crimes with";
   const msg1_2 = "DeepForensics";
   const msg2 = "The leading video forensics toolkit in the market.";
-  const msg3 = "Get Started";
+  const msg3 = "Get Started1";
   const dest = "/download";
   const web = image;
   return (
