1e37fd3a769e1e66f74fdce7833dc7b88e09c55f
affanmehmood <affanmehmood32@gmail.com>
2020-08-13 13:26:52 +0500

    compleated all pages

--- src/App.js
+++ src/App.js
@@ -10,19 +10,21 @@ import Contact from "./Contact";
 import NavBar from "./Navbar";
 import Footer from "./ReusableCompnents/footer.jsx";
 import "./App.css";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/download" component={Service} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-        <Redirect to="/" />
-      </Switch>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/download" component={Service} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Redirect to="/" />
+        </Switch>
+      </HashRouter>
       <Footer />
     </div>
   );
