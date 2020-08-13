48b3f3b6fcfbafae462405677ace375b6376e799
affanmehmood <affanmehmood32@gmail.com>
2020-08-13 22:49:21 +0500

    compleated all pages

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -11,39 +11,39 @@ const Sdata = [
     imgsrc: img1,
     title: "Object Recognition",
     text:
-      "Recognize a set of objects from the footage that need to be processed.",
+      "Recognize a set of objects from the footage \nthat need to be processed.",
   },
   {
     key: 2,
     imgsrc: img2,
     title: "Object Identification",
-    text: "Match the objects from the footage and the database.",
+    text: "Match the objects from the footage and \nthe database.",
   },
   {
     key: 3,
     imgsrc: img3,
     title: "Object Labeling",
-    text: "Assign labels to the desired objects that are recognized.",
+    text: "Assign labels to the desired objects that \nare recognized.",
   },
   {
     key: 4,
     imgsrc: img4,
     title: "Frame Enhancement",
-    text: "Apply filter(s) on a specific frame from a collection of filters.",
+    text: "Apply filter(s) on a specific frame from a \ncollection of filters.",
   },
   {
     key: 5,
     imgsrc: img5,
     title: "Report Generation",
     text:
-      "Compile all of the extracted information from evidence videos into a report.",
+      "Compile all of the extracted information from \nevidence videos into a report.",
   },
   {
     key: 6,
     imgsrc: img6,
     title: "User Friendly Frontend",
     text:
-      "Presenting the UI to the user in a easy to understand and use manner.",
+      "Presenting the UI to the user in a easy to \nunderstand and use manner.",
   },
 ];
 export default Sdata;
